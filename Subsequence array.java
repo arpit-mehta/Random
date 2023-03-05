cb943f882d46986e692cee16b367230179a0f439
arpit-mehta <91524040+arpit-mehta@users.noreply.github.com>
2023-03-05 16:57:43 +0530

    Update Subsequence array.java

--- Subsequence array.java
+++ Subsequence array.java
@@ -1,28 +1,20 @@
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        int[] arr= {1,2,3};
-        int n=3;
-        ArrayList<Integer> ans = new ArrayList<>();
-        printSubsequence(0,ans,arr,n);
-    }
-
-    private static void printSubsequence(int index, ArrayList<Integer> ans, int[] arr, int n) {
-        if(index==n){
-            for (Integer ele: ans) {
-                System.out.print(ele+" ");
-            }
-            System.out.println();
+class Solution {
+    public void generate(int[] nums,int i,List<Integer> ds,List<List<Integer>> ans){
+        if(i==nums.length){
+            ans.add(new ArrayList(ds));
             return;
         }
         //take or pick a particular index into subsequence
-        ans.add(arr[index]);
-        printSubsequence(index+1,ans,arr,n);
-        ans.remove(ans.size()-1); //while going back we also have to remove the added element
-
-        printSubsequence(index+1,ans,arr,n);
+        ds.add(nums[i]);
+        generate(nums,i+1,ds,ans); 
+        ds.remove(ds.size()-1); //while going back we also have to remove the added element
+        
+        //the element is not added to the subsequence
+        generate(nums,i+1,ds,ans);
     }
-
-
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        generate(nums,0,new ArrayList<>(),ans);
+        return ans;
+    }
 }
